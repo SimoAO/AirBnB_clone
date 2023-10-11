f6c4e657f9c0575d02d0343bfcbe30efc3b4bc27
abdo <abdoraja2005@gmail.com>
2023-10-11 14:38:13 +0100

    fix pycodestyle  for base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
@@ -23,23 +23,22 @@ class BaseModel:
         else:
             models.storage.new(self)
 
-
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime """
+        """updates the public instance attribute updated_at
+        with the current datetime """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance:"""
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance:"""
         dict_2 = self.__dict__.copy()
-        
+
         dict_2["created_at"] = self.created_at.isoformat()
         dict_2["updated_at"] = self.updated_at.isoformat()
         dict_2["__class__"] = self.__class__.__name__
         return dict_2
-    
-
