6e5038491c3d2558df45595f6b6322018f3f68d6
abdo <abdoraja2005@gmail.com>
2023-10-12 14:29:31 +0100

    fix pycodestyle

--- console.py
+++ console.py
@@ -17,16 +17,15 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     classes = {
-         "BaseModel": BaseModel,
-         "User" : User,
-         "State" : State,
-        "City" : City,
-        "Amenity" : Amenity,
-        "Place" : Place,
-        "Review" : Review,      
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review,
      }
 
-    
     def emptyline(self):
         """handle empty line + ENTER """
         pass
@@ -34,14 +33,15 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """ Quit command to exit the program """
         return True
-    
+
     def do_EOF(self, arg):
         """ if EOF exit the program """
         print()
         return True
 
     def do_create(self, arg):
-        """: Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        """ Creates a new instance of BaseModel
+        saves it (to the JSON file) and prints the id"""
         if len(arg) == 0:
             print("** class name missing **")
         elif arg not in self.__class__.classes.keys():
@@ -50,13 +50,14 @@ class HBNBCommand(cmd.Cmd):
             object_ = self.__class__.classes[arg]()
             object_.save()
             print(object_.id)
-            
+
     def do_show(self, args):
-        """ Prints the string representation of an instance based on the class name and id.
+        """ Prints the string representation of an instance
+          based on the class name and id.
           Ex: $ show BaseModel 1234-1234-1234."""
         arg = args.split()
         if len(arg) > 1:
-            key = arg[0]+ '.' + arg[1]
+            key = arg[0] + '.' + arg[1]
         if len(arg) == 0:
             print("** class name missing **")
         elif len(arg) <= 1:
@@ -69,12 +70,12 @@ class HBNBCommand(cmd.Cmd):
             print(f"{storage.all()[key]}")
 
     def do_destroy(self, args):
-        """ Deletes an instance based on the class name and id 
-            (save the change into the JSON file). 
+        """ Deletes an instance based on the class name and id
+            (save the change into the JSON file).
             Ex: $ destroy BaseModel 1234-1234-1234."""
         arg = args.split()
         if len(arg) > 1:
-            key = arg[0]+ '.' + arg[1]
+            key = arg[0] + '.' + arg[1]
         if len(arg) == 0:
             print("** class name missing **")
         elif len(arg) <= 1:
@@ -88,7 +89,8 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_all(self, args):
-        """Prints all string representation of all instances based or not on the class name.
+        """ Prints all string representation of all instances
+            based or not on the class name.
             Ex: $ all BaseModel or $ all."""
         list_of_instances = []
         arg = args.split()
@@ -105,18 +107,19 @@ class HBNBCommand(cmd.Cmd):
         print(list_of_instances)
 
     def do_update(self, args):
-        """  Updates an instance based on the class name and id by adding 
-            or updating attribute (save the change into the JSON file). 
+        """ Updates an instance based on the class name and id by adding
+            or updating attribute (save the change into the JSON file).
             Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
-            Usage: update <class name> <id> <attribute name> "<attribute value>"
+            Usage:
+            update <class name> <id> <attribute name> "<attribute value>"
         """
         arg = args.split()
         if len(arg) > 1:
-            key = arg[0]+ '.' + arg[1]
+            key = arg[0] + '.' + arg[1]
         if len(arg) == 0:
             print("** class name missing **")
         elif len(arg) == 1:
-            print("*** instance id missing **")
+            print("** instance id missing **")
         elif arg[0] not in self.__class__.classes.keys():
             print("** class doesn't exist **")
         elif key not in storage.all():
@@ -127,8 +130,9 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing **")
         else:
             instance = storage.all()[key]
-            setattr (instance, arg[2], arg[3])
+            setattr(instance, arg[2], arg[3])
             storage.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
