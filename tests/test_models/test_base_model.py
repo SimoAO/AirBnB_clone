c6be7976858d27ea3661810ca9ad1f8d8fb1f190
abdo <abdoraja2005@gmail.com>
2023-10-14 20:04:24 +0100

    fix pycodestyle errors

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Unittest for base_model """
-
-
-
 from models.base_model import BaseModel
 import unittest
 
@@ -12,7 +9,6 @@ class TestBaseModel(unittest.TestCase):
     def test_base_model_id(self):
         my_model = BaseModel()
         print(my_model.id)
-
 
 
 if __name__ == '__main__':
